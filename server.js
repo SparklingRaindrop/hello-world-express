4c1901ad2e2b629d99364be41b11fe8255ef38d9
Tsubasa <92432492+SparklingRaindrop@users.noreply.github.com>
2022-08-15 10:40:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 4000;
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Goodbye World!");
+  res.send(process.env.DAMMY_TEXT);
 });
 
 app.listen(PORT, () => {
